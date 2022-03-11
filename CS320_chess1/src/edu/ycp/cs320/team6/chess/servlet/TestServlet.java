bb36064b974b54a14685d8e3857b18f5c9c4d4b4
J.Kettula <jkettula@ycp.edu>
2022-03-11 15:05:56 -0500

    Created test servlet, jsp, main, launcher, webxml

---
+++ CS320_chess1/src/edu/ycp/cs320/team6/chess/servlet/TestServlet.java
@@ -0,0 +1,23 @@
+package edu.ycp.cs320.team6.chess.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class TestServlet extends HttpServlet{
+	
+private static final long serialVersionUID = 1L;
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		
+		System.out.println("Index Servlet: doGet");
+		
+		req.getRequestDispatcher("/_view/test.jsp").forward(req, resp);
+	}
+	
+}
