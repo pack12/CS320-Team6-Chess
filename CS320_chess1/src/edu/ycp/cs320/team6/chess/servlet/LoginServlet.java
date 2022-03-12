2757a97866daa4f33201bfb12ea8a88b353d3932
J.Kettula <jkettula@ycp.edu>
2022-03-11 21:24:58 -0500

    Added Login Page

---
+++ CS320_chess1/src/edu/ycp/cs320/team6/chess/servlet/LoginServlet.java
@@ -0,0 +1,22 @@
+package edu.ycp.cs320.team6.chess.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class LoginServlet extends HttpServlet{
+private static final long serialVersionUID = 1L;
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		
+		System.out.println("Login Servlet: doGet");
+		
+		req.getRequestDispatcher("/_view/login.jsp").forward(req, resp);
+	}
+
+}
---
+++ CS320_chess1/war/_view/login.jsp
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+
+<html>
+	<head>
+		<title>Login Page </title>
+	</head>
+
+	<body>
+		<form action="userlogin" method="post">
+                               User Name <input type="text" name="username"><br>
+                              Password <input type="password" name="password"><br>
+                           <input type="submit" value="login">
+           </form>
+	</body>
+</html>
