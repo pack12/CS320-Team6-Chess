0cae08fb563f21ad6707ed0ca47596565b7f045e
BeanIsGreat123 <bstinson1@ycp.edu>
2022-03-12 23:25:16 -0500

    Added Game controller and models
    Added incomplete controller(Game), and models(Piece and Board) for the game function. May have also messed up the java implementation, hence having John make a copy.

---
+++ CS320_chess1/src/edu/ycp/cs320/team6/chess/controller/Game.java
@@ -0,0 +1,79 @@
+package edu.ycp.cs320.team6.chess.controller;
+
+import edu.ycp.cs320.team6.chess.model.Board;
+
+
+public class Game{
+	
+	private static Board board;
+	private static int turn;
+	private static int clock;
+	
+	public Game() {
+		board = new Board();
+		turn = 1;
+		clock = 600;
+	}
+	
+	public void startGame() {
+		board.fillBoard();
+	}
+	
+	public void clearGame() {
+		board.clearBoard();
+	}
+	
+	//check color with turn to see if it is the player's turn
+	
+	public static boolean isPlayersTurn(int x, int y) {
+		if (board.getPiece(x, y).getColor() == "white" && turn == 1) {
+			return true;
+		}
+		else if (board.getPiece(x, y).getColor() == "black" && turn == 2) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+	
+	//check to see if the destination is on the board
+	
+	public static boolean isOnBoard(int x, int y) {
+		if (x<0 || x>7) {
+			return false;
+		}
+		else if (y<0 || y>7) {
+			return false;
+		}
+		else {
+			return true;
+		}
+	}
+	
+	//check to see if the move is legal
+	//ONLY INCLUDES: testing to see if the move is on the board and if it is the player's turn
+	
+	public static boolean validateMove(int xinit, int yinit, int xfinal, int yfinal) {
+		if(Game.isPlayersTurn(xinit, yinit) && Game.isOnBoard(xfinal, yfinal) ) {
+			return true;
+		}
+		else {
+			return false;
+		}	
+	}
+	
+	public static void updateTurn() {
+		if(turn == 1) {
+			turn = 2;
+		}
+		else if (turn == 2) {
+			turn = 1;
+		}
+	}
+	
+	public void makeMove(int xinit, int yinit, int xfinal, int yfinal) {
+		board.movePiece(xinit, yinit, xfinal, yfinal);
+		Game.updateTurn();
+	}
+}
---
+++ CS320_chess1/src/edu/ycp/cs320/team6/chess/model/Piece.java
@@ -0,0 +1,72 @@
+package edu.ycp.cs320.team6.chess.model;
+
+//model class for each piece
+
+public class Piece{
+
+	private String type;
+	private boolean blockCheck;
+	private int xLocation;
+	private int yLocation;
+	private String color;
+	private boolean onBoard;
+	
+public Piece(String type, boolean blockCheck, int xLocation, int yLocation, String color, boolean onBoard) {
+	this.blockCheck = blockCheck;
+	this.type = type;
+	this.color = color; 
+	this.xLocation = xLocation;
+	this.yLocation = yLocation;
+	this.onBoard = onBoard;
+}
+
+public int getX() {
+	return xLocation;
+}
+
+public void setX(char xLocation) {
+	this.xLocation = xLocation;
+}
+
+public int getY() {
+	return yLocation;
+}
+
+public void setY(char yLocation) {
+	this.yLocation = yLocation;
+}
+
+public String getColor() {
+	return color;
+}
+
+public void setColor(String color) {
+	this.color = color;
+}
+
+public boolean isOnBoard() {
+	return onBoard;
+}
+
+public void setOnBoard(boolean onBoard) {
+	this.onBoard = onBoard;
+}
+
+public String getType() {
+	return type;
+}
+
+public void setType(String type) {
+	this.type = type;
+}
+
+public boolean getBlockCheck() {
+	return blockCheck;
+}
+
+public void setBlockCheck(boolean blockCheck) {
+	this.blockCheck = blockCheck;
+}
+
+
+}
