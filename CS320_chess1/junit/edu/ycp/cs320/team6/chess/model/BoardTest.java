ed0208bf192171ddb51258b9913eb9ca7d32ac87
bmcbride1 <bmcbride1@ycp.edu>
2022-03-18 11:31:54 -0400

    Working Tests
    Piece test passing.
    Board test passing.
    Game test still failing.

--- CS320_chess1/junit/edu/ycp/cs320/team6/chess/model/BoardTest.java
+++ CS320_chess1/junit/edu/ycp/cs320/team6/chess/model/BoardTest.java
@@ -1,38 +1,38 @@
 package edu.ycp.cs320.team6.chess.model;
 
-import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.*;
+
 import org.junit.Before;
 import org.junit.Test;
 
 import edu.ycp.cs320.team6.chess.model.Board;
 
-class BoardTest {
-
+public class BoardTest {
 	private Board board;
-	
+
 	@Before
-	public void setup() {
+	public void setUp() {
 		board = new Board();
-		board.fillBoard();
-	}
-	
-	@Test
-	public void testHasPiece() {
-		assertTrue(board.hasPiece(0, 0));
-		assertFalse(board.hasPiece(4, 4));
+        board.fillBoard();
 	}
 
 	@Test
-	public void testGetPiece() {
-		board.removePiece(3, 0);
-		assertEquals(board.getPiece(3, 0), null);
-	}
-	
-	@Test
-	public void testClearBoard() {
-		assertTrue(board.hasPiece(1, 1));
-		board.clearBoard();
-		assertFalse(board.hasPiece(1, 1));
-	}
+    public void testHasPiece() {
+        assertTrue(board.hasPiece(0, 0));
+        assertFalse(board.hasPiece(4, 4));
+    }
+
+    @Test
+    public void testGetPiece() {
+        board.removePiece(3, 0);
+        assertEquals(board.getPiece(3, 0), null);
+    }
+
+    @Test
+    public void testClearBoard() {
+        assertTrue(board.hasPiece(1, 1));
+        board.clearBoard();
+        assertFalse(board.hasPiece(1, 1));
+    }
+
 }
