99c0e1c74e15b3201fa9b712455479994b4a531e
bmcbride1 <bmcbride1@ycp.edu>
2022-03-27 20:17:46 -0400

    A little bit of board

--- CS320_chess1/src/edu/ycp/cs320/team6/chess/controller/Game.java
+++ CS320_chess1/src/edu/ycp/cs320/team6/chess/controller/Game.java
@@ -1,6 +1,12 @@
 package edu.ycp.cs320.team6.chess.controller;
 
+import edu.ycp.cs320.team6.chess.model.Piece;
+import edu.ycp.cs320.team6.chess.model.Player;
+import edu.ycp.cs320.team6.chess.model.Board;
+
 public class Game{
+	
+	private 
 	
 	public Game() {
 		
--- CS320_chess1/src/edu/ycp/cs320/team6/chess/model/Board.java
+++ CS320_chess1/src/edu/ycp/cs320/team6/chess/model/Board.java
@@ -4,9 +4,13 @@ import edu.ycp.cs320.team6.chess.model.Player;
 import edu.ycp.cs320.team6.chess.model.Piece;
 
 public class Board{
+	private int[][] board;
 	
 	public Board(){
-		
+		board = new int[8][8];
 	}
 	
+	public int[][] getSpot(int i, int j) {
+		return 
+	}
 }
