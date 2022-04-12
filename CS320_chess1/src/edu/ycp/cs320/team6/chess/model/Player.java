0bce205166d693a30aaf1ce61358071c1cd00991
J.Kettula <jkettula@ycp.edu>
2022-04-12 09:12:12 -0400

    Changed some player.java methods around.

--- CS320_chess1/src/edu/ycp/cs320/team6/chess/model/Player.java
+++ CS320_chess1/src/edu/ycp/cs320/team6/chess/model/Player.java
@@ -6,6 +6,7 @@ import edu.ycp.cs320.team6.chess.model.Piece;
 
 public class Player{
 	private Piece[] piece;
+	private Piece playerPiece;
 	private ArrayList<Piece> offBoard;
 	
 	public Player(){
@@ -13,19 +14,42 @@ public class Player{
 		offBoard = new ArrayList<>();
 	}
 	
-	public Piece getPiece(int i) {
-		return piece[i];
-	}
 	
-	public void setPiece(int i, Piece piece) {
-		this.piece[i] = piece;
+	
+	//MK3 of setPiece
+	public void setPiece(int i) {
+		this.playerPiece = this.piece[i];
 	}
+
+//Bean: 
+	//I slightly changed some of the methods in the player class. Let me know what you think. 
+	//I kept your og methods in just in case you want to revert to them. I'm going to commit these, so hopefully it's okay. 
+	
+//MK1 of getPIece
+//	
+//	public Piece getPiece(int i) {
+//		return piece[i];
+//	}
+
+//MK1 of setPiece
+//	public void setPiece(int i, Piece piece) {
+//		this.piece[i] = piece;
+//	}
+
+	
+	//MK2
+//	public void setPiece(int i) {
+//		this.piece[i] = this.playerPiece;
+//	}
+	
+	
 	
 	public void pieceTaken(int i) {
 		offBoard.add(piece[i]);
 	}
 	
 	public void updatePiece() {
+		
 		//null
 	}
 }
