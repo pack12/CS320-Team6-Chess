4c42e612a340263313774e0a919a611558ed2bd8
BeanIsGreat123 <bstinson1@ycp.edu>
2022-05-10 22:14:01 -0400

    board method

--- CS320_chess1/junit/edu/ycp/cs320/team6/chess/model/BoardTest.java
+++ CS320_chess1/junit/edu/ycp/cs320/team6/chess/model/BoardTest.java
@@ -8,41 +8,12 @@ import org.junit.Test;
 import edu.ycp.cs320.team6.chess.model.Board;
 
 public class BoardTest {
-	private Board model;
+	private Board model = new Board();
+	
+	@Test
+	public void testGetFilePathByPosition() {
+		assertEquals("Images/Chess_black_rook.png", model.getImageFromPosition(8, 8));
+	}
 
-//	@Before
-//	public void setUp() {
-//		boardModel = new BoardModel();
-//        boardModel.fillBoard();
-//	}
-
-//	@Test
-//    public void testHasPiece() {
-//        assertTrue(boardModel.hasPiece(0, 0));
-//        assertFalse(boardModel.hasPiece(4, 4));
-//    }
-//
-//    @Test
-//    public void testGetPiece() {
-//        boardModel.removePiece(3, 0);
-//        assertEquals(boardModel.getPiece(3, 0), null);
-//    }
-//
-//    @Test
-//    public void testClearBoard() {
-//        assertTrue(boardModel.hasPiece(1, 1));
-//        boardModel.clearBoard();
-//        assertFalse(boardModel.hasPiece(1, 1));
-//    }
-    
-//   @Test
-//   public void testGetSpot() {
-//    	model = new Board();
-////    	assertTrue(model.getSpot(0, 0));
-//    	int x = model.getSpot(0, 0);
-//    	System.out.println(x);
-//
-//    	
-//    }
 
 }
--- CS320_chess1/src/edu/ycp/cs320/team6/chess/model/Board.java
+++ CS320_chess1/src/edu/ycp/cs320/team6/chess/model/Board.java
@@ -1,202 +1,295 @@
 package edu.ycp.cs320.team6.chess.model;
 
+
+
 import edu.ycp.cs320.team6.chess.model.Player;
 import edu.ycp.cs320.team6.chess.model.Piece;
 import edu.ycp.cs320.team6.chess.chessdb.persist.ChessDerbyDatabase;
 import edu.ycp.cs320.team6.chess.chessdb.model.DBPiece;
+
+
 
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Scanner;
 
+
+
 public class Board{
-	
-	
-	
-	ChessDerbyDatabase db = new ChessDerbyDatabase();
-	
-	private String[][] board;
-	
-	private Piece[][] board1;
-	
-	private Piece piece;
-	
-	
-	public Board(){
-		board = new String[8][8];
-		
-		
-	}
-	
-	
-	
-	
-	//Board is going to be able to get a piece depending on the location data inputed in
-	//BUT WHAT do we need? I need to be able to access pieces and their location data, and see if they match up with the Board array location. 
-	public void addPieceToBoard(String fileName, int x, int y) {
-		board[x][y] = fileName;
-		
-	}	
-	
-	
-	
-	public void fillBoardImage() throws FileNotFoundException {
-		File myFile = new File("chessBoardImages");
-		Scanner inputFile = new Scanner(myFile);
-		
-		
-		int row =0;
-		int col =0;
-//		int index = 0;
-		
-//		for(int i=0; i<piece.size(); i++) {
-//			board[row][col] = piece.get(i);
-//		}
-		
-		while(inputFile.hasNext()) {
-			String line = inputFile.nextLine();
-			
-			if(col ==8) {
-				break;
-			}
-//			System.out.println("row:"+row+" "+ "col:" + col+ line);
-			if(line.equals("")) {
-				line = inputFile.nextLine();
-//				System.out.println("This is the line that should be skipped"+ line);
-//				System.out.println("row:"+row+" "+ "col:" + col+ line);
-			}
-			board[row][col] = line;
-			
-			row++;
-			
-			if(row == 8) {
 
-				row=0;
-				col++;
-				
-				if(col==2) {
-					//6
-					row=0;
-					col=6;
-				}
-			}
-			
-		}
+ChessDerbyDatabase db = new ChessDerbyDatabase();
 
-//		System.out.println("BOARSEFSFSDFSDFDD"+board);
-		for(int x=0; x<board.length; x++) {
-			for(int y=0; y<board.length; y++) {
-				System.out.println("X: " + x + "Y:" + y + "SQUARESSS: " + getBoardImage(x, y));
-			}
-		}
-		
-		
-		
-		
-	}
-	
-	public void fillBoardSql() {
-		
-		int row = 0; 
-		int col = 0;
-		
-		ArrayList<DBPiece> piece = db.findUnCaptured("White");
-		
-		ArrayList<DBPiece> blackPieces = db.findUnCaptured("Black");
-		for(int i=0; i<piece.size(); i++) {
-			//System.out.println("WHHHHHHYYYYY"+piece.get(i).getX());
-			String filePath = translatePieceImage(piece.get(i).getX(), piece.get(i).getY());
-			//System.out.println("File from fillboardsql"+filePath);
-			
-			board[row][col] = filePath;
-			row++;
-			
-			if(col ==8) {
-				break;
-			}
-			if(row == 8) {
-				row = 0;
-				col++;
-				
-			}	
-			}
-		
-		row = 0;
-		col = 6;
-		for(int i=0; i<blackPieces.size(); i++) {
-			String filePath = translatePieceImage(blackPieces.get(i).getX(), blackPieces.get(i).getY());
-			//System.out.println("File from fillboardsql"+filePath);
-			
-			if(col ==8) {
-				break;
-			}
-			
-			board[row][col] = filePath;
-			row++;
-			
-			
-			if(row == 8) {
-				row = 0;
-				col++;
-				
-			}
-			
-			
-			
-			
-		}
-		for(int i1=0; i1<board.length; i1++) {
-			for(int j=0; j<board.length; j++) {
-				//System.out.println("FINal SQL BAoARD:  "+ " X: "+ i1+ "Y: "+ j + board[i1][j]);
-			}
-		}
-		
-		
-	}
-	
-	public void updateBoardImage(int oldX, int oldY, int newX, int newY) {
-		//System.out.println("This is old position X:"+ oldX+ "Y: " + oldY+board[oldX][oldY]);
-		//System.out.println("This is new position X: " + newX + "Y: " + newY);
-		
-		
-		board[newX][newY] = board[oldX][oldY];
-		board[oldX][oldY] = "";
-		
-		for(int i =0; i<board.length; i++) {
-			for(int j =0; j<board.length; j++) {
-				//System.out.println("What is this? X: " +i + " Y: "+ j+board[i][j]);
-			}
-		}
-	}
-	
-	public String translatePieceImage(int x, int y) {
-		//System.out.println("twas beforeX: " +(x) + " Y: "+ (y));
-//		System.out.println("twasX: " +(x+1) + " Y: "+ (y+1));
-		//System.out.println("FindArryaAcceptable Pos:   " + (x-1) + " " + (y-1));
-		DBPiece piece = db.findPieceByPosition(x, y);
-//		System.out.println("This is the database method to find piece:  " + piece.getType());
-//		System.out.println("This is the database method to find piece:  " + piece.getX());
-//		System.out.println("This is the database method to find piece:  " + piece.getY());
-		
-		String src = getBoardImage(piece.getX()-1, piece.getY()-1);
-		//System.out.println("PIECE FROM Board Array: "+src);
-		
-//		db.updatePiecePosition(x+1, y+1, newX+1, newY+1);
-		return src;
-	}
-	
-	public String getBoardImage(int x, int y) {
-		return board[x][y];
-	}
-	
-	
-	
-	public String getPiece(int x, int y) {
-		return board[x][y];
-	}
-	
-	
-	
-	
+private String[][] board;
+
+public Board(){
+
+board = new String[8][8];
+
+
+
+
+
 }
+
+
+
+
+
+
+
+
+
+//Board is going to be able to get a piece depending on the location data inputed in
+
+//BUT WHAT do we need? I need to be able to access pieces and their location data, and see if they match up with the Board array location. 
+
+public void addPieceToBoard(String fileName, int x, int y) {
+
+board[x][y] = fileName;
+
+
+
+} 
+
+
+
+
+
+
+
+public void fillBoardImage() throws FileNotFoundException {
+
+	File myFile = new File("chessBoardImages");
+
+	Scanner inputFile = new Scanner(myFile);
+
+
+
+
+
+	int row =0;
+
+	int col =0;
+
+
+
+
+
+	while(inputFile.hasNext()) {
+
+		String line = inputFile.nextLine();
+
+
+
+		if(col ==8) {
+
+			break;
+
+}
+
+
+
+if(line.equals("")) {
+
+line = inputFile.nextLine();
+
+
+
+}
+
+board[row][col] = line;
+
+
+
+row++;
+
+
+
+if(row == 8) {
+
+
+
+row=0;
+
+col++;
+
+
+
+if(col==2) {
+
+//6
+
+row=0;
+
+col=6;
+
+}
+
+}
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
+public void fillBoardSql() {
+
+
+
+int row = 0; 
+
+int col = 0;
+
+
+
+ArrayList<DBPiece> piece = db.findUnCaptured("White");
+
+
+
+ArrayList<DBPiece> blackPieces = db.findUnCaptured("Black");
+
+
+
+
+for(int i=0; i<piece.size(); i++) {
+
+
+String filePath = translatePieceImage(piece.get(i).getX(), piece.get(i).getY());
+
+
+board[row][col] = filePath;
+
+row++;
+
+
+if(row == 8) {
+
+row = 0;
+
+col++;
+
+
+
+}
+
+if(col==3) {
+
+break;
+
+}
+
+}
+
+
+
+for(int i=0; i<board.length; i++) {
+
+for(int j=0; j<board.length; j++) {
+
+System.out.println("WRONG X: "+ i+ "Y" + j+ "  "+getBoardImage(i,j));
+
+}
+
+}
+
+
+
+row = 0;
+
+col = 7;
+
+
+
+
+
+
+
+for(int i=0; i<blackPieces.size(); i++) {
+
+String filePath = translatePieceImage(blackPieces.get(i).getX(), blackPieces.get(i).getY());
+
+
+System.out.println("Black fill in X: " + blackPieces.get(i).getX() + "Black fill in Y: " + blackPieces.get(i).getY()+ "Black PIece? " + filePath + blackPieces.get(i).getType());
+
+
+board[row][col] = filePath;
+
+row++;
+
+
+
+
+
+if(row == 8) {
+
+row = 0;
+
+col-=1;
+
+}
+
+}
+
+
+}
+
+
+public String translatePieceImage(int x, int y) {
+
+
+DBPiece piece = db.findPieceByPosition(x, y);
+
+
+String src = getBoardImage(piece.getX()-1, piece.getY()-1);
+
+
+
+return src;
+
+}
+
+public String getImageFromPosition(int x, int y) {
+
+	String filePath = "Images/Chess_black_pawn.png";
+	
+	DBPiece piece = db.findPieceByPosition(x, y);
+	
+	if(piece!=null) {
+	String color = piece.getColor().toLowerCase();
+	String type = piece.getType().toLowerCase();
+	
+	filePath = "Images/Chess_" + color + "_" + type + ".png";
+	}
+
+	return filePath;	
+}
+
+
+
+public String getBoardImage(int x, int y) {
+
+return board[x][y];
+
+}
+
+
+}
+
